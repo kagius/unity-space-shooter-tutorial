9aace7941227191e93a3b6573834a353c181c409
kagius <karl.agius@gmail.com>
2015-03-06 17:26:35 +0100

    08: Shooting shots

--- Assets/Scripts/PlayerController.js
+++ Assets/Scripts/PlayerController.js
@@ -13,6 +13,20 @@ class PlayerController extends MonoBehaviour {
 	var speed : float;
 	var tilt : float;
 	var boundary: Boundary;
+	var shot: GameObject;
+	var shotSpawn: Transform;
+	var fireRate : float;
+
+	private var nextFire : float;
+
+	function Update() {
+		
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+	    {
+	        nextFire = Time.time + fireRate;
+	        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);	
+	    }
+	}
 
 	function FixedUpdate () {
 

8df6f98f411565a87e07c888db7937c578143c73
kagius <karl.agius@gmail.com>
2015-03-06 15:52:38 +0100

    06: Moving the player's ship

---
+++ Assets/Scripts/PlayerController.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+class Boundary
+{
+    var xMin : float;
+    var xMax : float;
+    var zMin : float;
+    var zMax : float;
+}
+
+class PlayerController extends MonoBehaviour {
+
+	var speed : float;
+	var tilt : float;
+	var boundary: Boundary;
+
+	function FixedUpdate () {
+
+		var moveHorizontal : float= Input.GetAxis ("Horizontal");
+		var moveVertical : float= Input.GetAxis ("Vertical");
+		
+		var movement : Vector3= new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		rigidbody.velocity = movement * speed;
+		
+		rigidbody.position = new Vector3(
+			Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax), 
+			0.0, 
+			Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax));
+			
+		rigidbody.rotation = Quaternion.Euler(0.0, 0.0, rigidbody.velocity.x * -tilt);
+	}
+}

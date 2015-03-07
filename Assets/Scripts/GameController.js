1e4817cd53fdd6050d610c59b73334509a9c8353
kagius <karl.agius@gmail.com>
2015-03-07 23:37:38 +0100

    12: Game controller

---
+++ Assets/Scripts/GameController.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var hazard: GameObject;
+var spawnValues : Vector3;
+
+function spawnWaves() : void {
+	Instantiate(
+		hazard, 
+		new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z), 
+		Quaternion.identity);
+}
+
+function Start() {
+	spawnWaves();
+}

447483614ce6997fb46543bd63d9120c0e738c7b
kagius <karl.agius@gmail.com>
2015-03-08 00:52:40 +0100

    13: Spawning waves

--- Assets/Scripts/DestroyedByContact.js
+++ Assets/Scripts/DestroyedByContact.js
@@ -5,12 +5,14 @@ var playerExplosion : GameObject;
 
 function OnTriggerEnter(other: Collider) {
 
-	if (other.tag == "Boundary")
+	if (other.tag == "Boundary") {
 		return;
+	}
 	
-	if (other.tag == "Player")
+	if (other.tag == "Player") {
 		Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
-			
+	}
+	
 	Instantiate(explosion, transform.position, transform.rotation);
 	Destroy(other.gameObject);
     Destroy(gameObject);
--- Assets/Scripts/GameController.js
+++ Assets/Scripts/GameController.js
@@ -1,13 +1,26 @@
 ﻿#pragma strict
 
 var hazard: GameObject;
-var spawnValues : Vector3;
+var hazardCount: int;
 
-function spawnWaves() : void {
-	Instantiate(
-		hazard, 
-		new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z), 
-		Quaternion.identity);
+var spawnValues : Vector3;
+var startWait: float;
+var spawnWait: float;
+var waveWait: float;
+
+function spawnWaves() {
+ 	yield WaitForSeconds (startWait);
+    while (true)
+    {
+		for (var i:int = 0; i < hazardCount; i++) {
+			Instantiate(
+				hazard, 
+				new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z), 
+				Quaternion.identity);
+			yield WaitForSeconds (spawnWait);
+		}
+		yield WaitForSeconds (waveWait);
+	}
 }
 
 function Start() {
